3aaad4a536a53d0be6ccf72781179dc53156e6e3
oddeven <arunbhagat132@gmail.com>
2022-08-19 14:35:06 +0530

    day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/loginpage.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 void main(List<String> args) {
   runApp(new myapp());
@@ -16,15 +18,19 @@ class myapp extends StatelessWidget {
       
       themeMode: ThemeMode.light,
       theme: ThemeData(
-        primarySwatch:Colors.deepPurple
+        primarySwatch:Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        primaryTextTheme: GoogleFonts.latoTextTheme(),
+       
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark
 
       ),
-      initialRoute: "/home",
+      initialRoute: "/loginpage",
 routes:{
   "/":(context) =>loginpage(),
+  
    "/home":(context) =>homepage(),
   "/login":(context) => loginpage()
   
--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -6,18 +6,53 @@ class loginpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "login page",
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
-          ),
-          textScaleFactor:2
-          ,
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login@image.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "ODDEVEN21",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter username",
+                      labelText: "Usename",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    child: Text("Login"),
+                    onPressed: () {
+                      print("oddeven");
+                    },
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
